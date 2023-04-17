ff59d9a9f6244a8545c424dffadb806813b52258
Charles Graham <crg0905@gmail.com>
2023-04-17 15:53:35 +0900

    Updated import to reflect name change of files

--- src/components/HighlightTooltip/HighlightTooltip.jsx
+++ src/components/HighlightTooltip/HighlightTooltip.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './ToolTip.css';
+import './HighlightTooltip.css';
 
 function HighlightTooltip({ text, summary, tags }) {
   return (
