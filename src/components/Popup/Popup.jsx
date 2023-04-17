d4b03c5eaeb35a3344635562a8990db90c93b9db
Charles Graham <crg0905@gmail.com>
2023-04-17 15:52:24 +0900

    Removed Article import

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Article from './Article';
 import HighlightsList from '../HighlightsList/HightlightsList';
 import HighlightTooltip from '../HighlightTooltip/HighlightTooltip';
 
