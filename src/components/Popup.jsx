fbcf00d968b2d3f7981ab78c3ff3c17a4b7d5b6d
Charles Graham <crg0905@gmail.com>
2023-04-17 14:52:59 +0900

    Removed unused files, added button to Popup.jsx

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Popup() {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const handleToggle = () => {
+    setIsEnabled(!isEnabled);
+    // Call a function here to enable or disable the extension
+  };
+  
   return (
     <div>
       <h1>Article Summary</h1>
       <p>Selected text: <span id="selectedText"></span></p>
       <button id="summaryButton">Get summary</button>
+      <button onClick={handleToggle}>{isEnabled ? 'Disable' : 'Enable'}</button>
     </div>
   );
 }
